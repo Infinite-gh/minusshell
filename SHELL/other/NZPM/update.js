9fdf785d29f6b2c8ca79ab9339edea4b3237ac13
nrdsstuff <nrdfoss@gmail.com>
2022-01-27 10:51:08 +0100

    fixed an old bug caused by refreshing

--- SHELL/other/NZPM/update.js
+++ SHELL/other/NZPM/update.js
@@ -1,15 +1,12 @@
-module.exports = async () =>{
+module.exports = () =>{
 
     const shell = require('shelljs')
 
     const NZTK = require('../NZTK')
     
     shell.exec(`git clone https://github.com/Infinite-gh/ICMD-repo ./SHELL/temp/NZPM`)
-    await shell.exec(`rm ./SHELL/configs/NZPM/packages.json`)
-    await shell.exec(`rm ./SHELL/configs/NZPM/packagelist.txt`)
-    await shell.exec(`mv ./SHELL/temp/NZPM/packages.json ./SHELL/configs/NZPM/.`)
-    await shell.exec(`mv ./SHELL/temp/NZPM/packagelist.txt ./SHELL/configs/NZPM/.`)
-    await shell.exec(`rm -rf ./SHELL/temp/NZPM`)
+    NZTK.moveFile('./SHELL/temp/NZPM/packages.json', "./SHELL/configs/NZPM/packages.json", " ", true)
+    NZTK.moveFile('./SHELL/temp/NZPM/packagelist.txt', "./SHELL/configs/NZPM/packagelist.txt", " ", true)
 
-    await NZTK.log(`updated the repository`, `NZPM`, `refresh`)
+    NZTK.log(`updated the repository`, `NZPM`, `refresh`)
 }
